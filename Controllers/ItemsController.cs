33ac14920327716d60a3fe447e8121015a0587b4
abdurafeekp1 <32807968+rafeeqkp1@users.noreply.github.com>
2018-04-15 14:48:47 +0400

    item

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessExcel.Controllers
+{
+    public class ItemsController : Controller
+    {
+        //
+        // GET: /Items/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
