8de7b951d182183af5b993d6a3fffc2c9db1089d
abdurafeekp1 <abdurafeeqkp1@gmail.com>
2018-06-11 14:35:46 +0400

    rafe

--- Providers/ProviderContext/SalesManageDataContext.TargetAchievementView.cs
+++ Providers/ProviderContext/SalesManageDataContext.TargetAchievementView.cs
@@ -113,12 +113,38 @@ namespace BusinessExcel.Providers.ProviderContext
 
         internal object GetTargetAchievementViewPagingExport(ActionViewFilters Filters)
         {
+            // var res = this.TargetAchievementView.Select(x => x);
 
-            if (Filters == null)
-                Filters = new Models.ActionViewFilters() { ItemCode = "" };
-            var res = from x in this.TargetAchievementView
-                          // where (String.IsNullOrEmpty(Filters.) || x.Item == Filters.ItemCode)
-                      select new { Name=x.Name, USER_NAME = x.UserName,Location=x.Location,FromDate=x.fromdate,ToDate=x.todate,Target=x.target_amt, Achieved=x.achieved_amt };
+
+            var res = this.TargetAchievementView.Select(x => new { Name = x.Name, USER_NAME = x.UserName, Location = x.Location, FromDate = x.fromdate, ToDate = x.todate, Target = x.target_amt, Achieved = x.achieved_amt });
+      
+
+            if (Filters.StartDate != default(DateTime))
+            {
+                res = res.Where(x => x.FromDate == Filters.StartDate);
+            }
+            if (Filters.EndDate != default(DateTime))
+            {
+                res = res.Where(x => x.ToDate == Filters.EndDate);
+
+            }
+
+            if (!string.IsNullOrEmpty(Filters.UserName))
+                res = res.Where(x => x.USER_NAME == Filters.UserName);
+
+       
+
+            if (!string.IsNullOrEmpty(Filters.Location))
+            {
+                var temp = this.getLocationDetail(Filters.Location).description;
+                res = res.Where(x => x.Location.ToString() == temp);
+            }
+
+            //if (Filters == null)
+            //    Filters = new Models.ActionViewFilters() { ItemCode = "" };
+            //var res = from x in this.TargetAchievementView
+            //              where ( x.UserName==Filters.UserName || x.Location==Filters.Location)
+            //          select new { Name=x.Name, USER_NAME = x.UserName,Location=x.Location,FromDate=x.fromdate,ToDate=x.todate,Target=x.target_amt, Achieved=x.achieved_amt };
             return res.ToList();
         }
     }
