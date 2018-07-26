1106c3c8b36c46f3626d9b44cf4ab8a5d2b11357
Sujith Radhakrishnan <sujithvakkom@gmail.com>
2018-07-26 17:24:23 +0400

    Tra

---
+++ Models/AlertModal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessExcel.Models
+{
+    public class AlertModal
+    {
+        public string TriggerDisplayName { get; set; }
+        public string ModalID { get; set; }
+        public string ModalLabelID { get; set; }
+        public string ModalTitle { get; set; }
+        public string Message { get; set; }
+        public List<AlertButton> Buttons { get; set; }
+    }
+
+    public class AlertButton
+    {
+        public string ButtonDisplayName { get; set; }
+        public string LoadingElementId { get; set; }
+        public IDictionary<string, object> buttonAttributes { get; set; }
+        public string Image { get; set; }
+    }
+
+}
